6aceaa9cae29066f8f5512e8a9fe6838d5bf75da
Anastasiia Hress <nastia.gress@gmail.com>
2021-05-11 17:13:01 +0300

    small fixes

--- src/components/SingleTrip.js
+++ src/components/SingleTrip.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import sanityClient from '../client.js'
-import imageUrlBuilder from '@sanity/image-url'
 import BlockContent from '@sanity/block-content-to-react'
 
-const builder = imageUrlBuilder(sanityClient)
-function urlFor(source) {
-  return builder.image(source)
-}
 
 export default function SingleTrip() {
   const [singlePost, setSinglePost] = useState(null)
